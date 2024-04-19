7b08aa78f5bca34044944e0c864863f997f49e88
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-19 04:12:06 -0500

    Change structure order to show login view

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from 'react';
+import { AppProvider } from './core/state/AppContext';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routes';
+import './global.css';
 
 export const App = (): ReactElement => {
   return (
-    <></>
+    <>
+      <AppProvider>
+        <RouterProvider router={router}/>
+      </AppProvider>
+    </>
   );
 };
--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -4,23 +4,31 @@ import { Login } from '../pages/Login';
 import { Home } from '../pages/Home';
 import { Register } from '../pages/Register';
 import { ProtectLoggedRoute } from './ProtectLoggedRoute';
+import { LayoutAuth } from '../ui/layouts/LayoutAuth';
 
 export const router = createHashRouter([
-  {
-    path: 'auth',
-    element: 
-    <ProtectLoggedRoute >
-      <Login />
-    </ProtectLoggedRoute>
 
-  },
   {
-    path: '/register',
-    element: 
-    <ProtectLoggedRoute >
-      <Register/>
-    </ProtectLoggedRoute>
+    path: '',
+    Component: LayoutAuth,
+    children: [
+      {
+        path: 'auth',
+        element: 
+        <ProtectLoggedRoute >
+          <Login />
+        </ProtectLoggedRoute>
     
+      },
+      {
+        path: '/register',
+        element: 
+        <ProtectLoggedRoute >
+          <Register/>
+        </ProtectLoggedRoute>
+        
+      },
+    ]
   },
   {
     path: '',
