5838d860be0ac520fb662914b39b21651e6c87bc
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-19 04:25:16 -0500

    Change structure order to display register view

--- src/app/pages/Register.tsx
+++ src/app/pages/Register.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from 'react';
-import { FormRegister } from '../ui/forms/Register';
+import { RegisterContainer } from '../containers/RegisterContainer';
 
 export const Register = (): ReactElement => {
-  return <section>
-    <h1>Register</h1>
-    <FormRegister />
-  </section>;
+  return(
+    <>
+      <RegisterContainer />
+    </>
+  );
 };
