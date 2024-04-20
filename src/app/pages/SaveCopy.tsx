db8c97cfcad96e77fab13b4ebdbbd0a59467e74e
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-19 19:20:22 -0500

    Add container and pages for the first functionality view

---
+++ src/app/pages/SaveCopy.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react';
+import { SaveCopyContainer } from '../containers/SaveCopyContainer';
+
+export const SaveCopy = (): ReactElement => {
+  return(
+    <>
+      <SaveCopyContainer />
+    </>
+  );
+};
