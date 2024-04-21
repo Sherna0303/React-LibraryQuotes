5a575bbb33d3fabd1eca8d4ca9fc39cc75c47a7b
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-21 03:59:32 -0500

    Add span element, add style for login form

---
+++ src/app/ui/elements/Span/index.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react';
+import './style.css';
+
+interface SpanProps {
+  className: string;
+  text: string;
+  
+}
+
+export const Span = ({ className, text }: SpanProps): ReactElement => {
+
+  return (
+    <span className={className}>
+      {text}
+    </span>
+  );
+};
