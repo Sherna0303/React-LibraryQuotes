a177a5ab950aaa29061e0ca2d85f150c56f11088
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-21 07:08:02 -0500

    Refactor save copy form, add styles for save copy form

--- src/app/ui/forms/SaveCopy/index.tsx
+++ src/app/ui/forms/SaveCopy/index.tsx
@@ -1,65 +1,102 @@
-import { FormEvent, ReactElement, useState } from 'react';
-import { Legend } from '../../elements/Legend';
+import { FormEvent, ReactElement, useEffect, useState } from 'react';
+import { useSaveCopy } from '../../../core/hooks/useSaveCopy';
 import { Label } from '../../elements/Label';
 import { Button } from '../../elements/Button';
-import { useSaveCopy } from '../../../core/hooks/useSaveCopy';
+import './style.css';
+import { Span } from '../../elements/Span';
 import { Options } from '../../elements/Options';
 
-interface typeData {
-  type: string;
+interface FormData {
+  name?: string;
+  author?: string;
+  price?: number;
+  type?: string;
 }
-
-const initialFormState: typeData = {
-  type: 'BOOK',
-};
-
 
 export const FormSaveCopy = (): ReactElement => {
   const { saveCopy, error } = useSaveCopy();
-  const [typeData, setTypeData] = useState<typeData>(initialFormState);
+  const [formData, setFormData] = useState<FormData>();
+  const [nameError, setNameError] = useState<string | null>(null);
+  const [authorError, setAuthorError] = useState<string | null>(null);
+  const [priceError, setPriceError] = useState<string | null>(null);
+  const [isFormValid, setIsFormValid] = useState<boolean>(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setTypeData({ ...typeData, [name]: value });
+  const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = event.target.value;
+    setFormData((prevData) => ({ ...prevData, name: newName }));
+
+    if (newName.length < 3) {
+      setNameError('El nombre debe tener al menos 3 caracteres');
+    } else {
+      setNameError(null);
+    }
+  };
+
+  const handleChangeAuthor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newAuthor = event.target.value;
+    setFormData((prevData) => ({ ...prevData, author: newAuthor }));
+
+    if (newAuthor.length < 3) {
+      setAuthorError('El autor debe tener al menos 3 caracteres');
+    } else {
+      setAuthorError(null);
+    }
+  };
+
+  const handleChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPrice = Number(event.target.value);
+    setFormData((prevData) => ({ ...prevData, price: newPrice }));
+
+    if (newPrice <= 0) {
+      setPriceError('El precio debe ser mayor que 0');
+    } else {
+      setPriceError(null);
+    }
+  };
+
+  const handleChangeType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newType = event.target.value;
+    setFormData((prevData) => ({ ...prevData, type: newType }));
   };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const target = event.target as typeof event.target & {
-          elements: {
-            name: { value: string }
-            author: { value: string };
-            price: { value: number };
-            type: { value: string };
-          };
-        };
-        
-    const name = target.elements.name.value;
-    const author = target.elements.author.value;
-    const price = target.elements.price.value;
-    const type = target.elements.type.value;
 
+    if (!isFormValid || !formData) {
+      return;
+    }
+
+    const { name = '', author = '', price = 0, type = 'BOOK' } = formData;
     saveCopy(name, author, price, type);
   };
 
+  useEffect(() => {
+    if (!formData) return;
+
+    const { name = '', author = '', price = 0 } = formData;
+
+    setIsFormValid(!(name.length < 3 || author.length < 3 || price <= 0));
+  }, [formData]);
+
   return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <Legend className='main__legend' text='Register credentials'/>
-        <Label classNameLabel='main__label' classNameInput='main__input' nameInput='name' type='text' text='Name:' />
-        <Label classNameLabel='main__label' classNameInput='main__input' nameInput='author' type='text' text='Author:' />
-        <Label classNameLabel='main__label' classNameInput='main__input' nameInput='price' type='number' text='Price:' />
+    <form onSubmit={handleSubmit} className='main__form main__form-copy'>
+      <fieldset className='main__fieldset'>
+        <Label classNameLabel='main__label main__label-copy' classNameSpan='main__span' classNameInput={`main__input ${nameError ? 'error' : ''}`} nameInput='name' type='text' text='Name:' onChange={handleChangeName} />
+        {nameError && <Span className='main__error' text={nameError} />}
+        <Label classNameLabel='main__label main__label-copy' classNameSpan='main__span' classNameInput={`main__input ${authorError ? 'error' : ''}`} nameInput='author' type='text' text='Author:' onChange={handleChangeAuthor} />
+        {authorError && <Span className='main__error' text={authorError} />}
+        <Label classNameLabel='main__label main__label-copy' classNameSpan='main__span' classNameInput={`main__input ${priceError ? 'error' : ''}`} nameInput='price' type='number' text='Price:' onChange={handleChangePrice} />
+        {priceError && <Span className='main__error' text={priceError} />}
         
-        <label className='main__label'>Type:
-          <select className='main__select' id="type" name="type" value={typeData.type} onChange={handleChange}>
+        <label className='main__label main__label-copy'>Type:
+          <select className='main__select' id="type" name="type" value={formData?.type || ''} onChange={handleChangeType}>
             <Options className='main__option' value='BOOK' text='Book'/>
             <Options className='main__option' value='NOVEL' text='Novel'/>
           </select>
         </label>
-
       </fieldset>
-      <Button className='main__button' text='Send'/>
-      {error && <span style={{ color: 'red' }}>{error}</span>}
+      {error && <Span className={`main__${error === 'Guardado satisfactoriamente' ? 'success' : 'error'}`} text={error}/>}
+      <Button className='main__button' text='Save Copy' disabled={!isFormValid} />
     </form>
   );
 };
