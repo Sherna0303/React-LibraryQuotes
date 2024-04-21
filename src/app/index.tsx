8a750543f17afeb27a5401e88fe34bdb92575827
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-21 04:06:59 -0500

    Add source, Change styles path

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { AppProvider } from './core/state/AppContext';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 import './ui/styles/global.css';
+import './ui/styles/normalize.css';
 
 export const App = (): ReactElement => {
   return (
