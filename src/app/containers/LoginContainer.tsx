4c515ac18912b8a7ec6b08309a86410cc289031c
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-21 04:37:07 -0500

    Fix class names in login container to reuse them in register

--- src/app/containers/LoginContainer.tsx
+++ src/app/containers/LoginContainer.tsx
@@ -6,14 +6,14 @@ import { LinkRoute } from '../ui/elements/LinkRoute';
 export const LoginContainer = (): ReactElement => {
   return (
     <div className='login__container'>
-      <Title text="Login" className='login__Title' type='h1' />
+      <Title text="Login" className='main__title' type='h1' />
       <FormLogin />
-      <div className="separator">
-        <span className="separator-line" />
-        <span className="or">or</span>
-        <span className="separator-line" />
+      <div className="main__separator">
+        <span className="main__separator-line" />
+        <span className="main__or">or</span>
+        <span className="main__separator-line" />
       </div>
-      <LinkRoute route='/register' text='Register' />
+      <LinkRoute className='main__link' route='/register' text='Register' />
     </div>
   );
 };
