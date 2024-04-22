edafce3201b81652cb3f3793b67aa630cd92839d
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-21 22:09:38 -0500

    Add style for book quote receipt

--- src/app/ui/components/PriceListResponse/CopyDetails.tsx
+++ src/app/ui/components/PriceListResponse/CopyDetails.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Paragraph } from '../../elements/Paragraph';
+import { Span } from '../../elements/Span';
 
 interface CopyDetailsProps {
   copy: {
@@ -14,11 +15,27 @@ interface CopyDetailsProps {
 export const CopyDetails: React.FC<CopyDetailsProps> = ({ copy }) => {
   return (
     <>
-      <Paragraph className='copies__name' text={`Name: ${copy.name}`} />
-      <Paragraph className='copies__author' text={`Author: ${copy.author}`} />
-      <Paragraph className='copies__price' text={`Price: ${copy.price.toFixed(2)}`} />
-      <Paragraph className='copies__discount' text={`Discount: ${copy.discount.toFixed(2)}`} />
-      <Paragraph className='copies__totalPrice' text={`Total Price: ${copy.totalPrice.toFixed(2)}`} />
+      <Paragraph className='copies__name' text={copy.name} />
+
+      <div className="copies__detail-container">
+        <Span className='copies__detail' text='Author:'/>
+        <Paragraph className='copies__author' text={copy.author} />
+      </div>
+
+      <div className="copies__detail-container">
+        <Span className='copies__detail' text='Price:'/>
+        <Paragraph className='copies__price' text={copy.price.toFixed(2)}/>
+      </div>
+      
+      <div className="copies__detail-container">
+        <Span className='copies__detail' text='Discount:'/>
+        <Paragraph className='copies__discount' text={copy.discount.toFixed(2)} />
+      </div>
+
+      <div className="copies__detail-container">
+        <Span className='copies__detail' text='Total Price:'/>
+        <Paragraph className='copies__totalPrice' text={copy.totalPrice.toFixed(2)} />
+      </div>
     </>
   );
 };
--- src/app/ui/components/PriceListResponse/index.tsx
+++ src/app/ui/components/PriceListResponse/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { CopyDetails } from './CopyDetails';
 import { Title } from '../../elements/Title';
 import { CopyListDetail } from '../../../core/models/copy-list-price-response.model';
+import './style.css';
+import { Span } from '../../elements/Span';
+import { Paragraph } from '../../elements/Paragraph';
 
 export const PriceListResponse: React.FC<CopyListDetail> = ({ responseData }) => {
   if (!responseData.copies) {
@@ -11,10 +14,24 @@ export const PriceListResponse: React.FC<CopyListDetail> = ({ responseData }) =>
   return (
     <>
       <Title className='result__title' text='Result' type='h2'/>
-      <Title className='result__antiquityYears' text={`Seniority Years: ${responseData.antiquityYears}`} type='h4'/>
-      <Title className='result__total' text={`Total: ${responseData.total.toFixed(2)}`} type='h4'/>
-      <Title className='result__totalDiscount' text={`Total Discount: ${responseData.totalDiscount.toFixed(2)}`} type='h4'/>
+
+      <div className="result__detail-container">
+        <Span className='result__detail' text='Seniority Years:'/>
+        <Paragraph className='result__antiquityYears' text={responseData.antiquityYears.toString()}/>
+      </div>
+
+      <div className="result__detail-container">
+        <Span className='result__detail' text='Total:'/>
+        <Paragraph className='result__total' text={responseData.total.toFixed(2)}/>
+      </div>
+
+      <div className="result__detail-container">
+        <Span className='result__detail' text='Total Discount:'/>
+        <Paragraph className='result__totalDiscount' text={responseData.totalDiscount.toFixed(2)}/>
+      </div>
+
       <Title className='result__title-copies' text='Copies:' type='h2'/>
+
       <div className='result_copies'>
         {responseData.copies.map((copy, index) => (
           <CopyDetails key={ copy.name+''+index} copy={copy} />
